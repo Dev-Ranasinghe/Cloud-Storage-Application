0a542d0d4ca44c448baaf87e09296d018f5d7ae8
Dev Ranasinghe <999sasmith@gmail.com>
2025-03-22 00:12:21 +0530

    shadcn form added

---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type FormType = "signin" | "signup";
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
