829b6ea5b04ef8ea350d272e2463eab7be33fb5d
Dev Ranasinghe <999sasmith@gmail.com>
2025-03-22 09:39:26 +0530

    root layout added

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const dynamic = "force-dynamic";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <main className="flex h-screen"></main>;
+};
+export default Layout;
