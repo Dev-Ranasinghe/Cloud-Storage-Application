be9ebb791d31b604b74b6cc0ca20902d67ef6204
Dev Ranasinghe <999sasmith@gmail.com>
2025-03-22 09:45:05 +0530

    header sidebar mobile nav added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-export const dynamic = "force-dynamic";
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="flex h-screen"></main>;
+  return (
+    <main className="flex h-screen">
+      Sidebar
+      <section className="flex h-full flex-1 flex-col">
+        Mobile Navigation Header
+        <div className="main-content">{children}</div>
+      </section>
+    </main>
+  );
 };
 export default Layout;
